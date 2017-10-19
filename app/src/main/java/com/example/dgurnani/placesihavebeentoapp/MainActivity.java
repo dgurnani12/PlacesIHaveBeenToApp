2dc1feb616a0a937849ba888318ccfa792388c22
dgurnani <gurnani.dinesh@gmail.com>
2017-10-18 22:47:41 -0400

    further in development of this project TODO: Fix map bugs as indicated

--- app/src/main/java/com/example/dgurnani/placesihavebeentoapp/MainActivity.java
+++ app/src/main/java/com/example/dgurnani/placesihavebeentoapp/MainActivity.java
@@ -6,10 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
-
 import com.google.android.gms.maps.model.LatLng;
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,14 +17,22 @@ public class MainActivity extends AppCompatActivity {
     static ArrayList<LatLng> locations = new ArrayList<>();
     static ArrayAdapter arrayAdapter;
 
+    private Button friendsButton;
+    private Button socialMediaButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        friendsButton = (Button) findViewById(R.id.FriendsButton);
+        socialMediaButton = (Button) findViewById(R.id.SocialMediaButton);
+
         ListView listView = (ListView) findViewById(R.id.listView);
 
-        places.add("Add a new 'PlaceIHaveBeenTo'");
+        places.add("Add a new Place by tapping here!");
+        locations.add(new LatLng(0, 0));
 
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, places);
         listView.setAdapter(arrayAdapter);
@@ -39,6 +46,22 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public void HandleMediaButtons(View view) {
+
+        Intent intent;
+
+        switch (view.getId()) {
+            case (R.id.FriendsButton):
+                intent = new Intent(getApplicationContext(), FriendViewActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.SocialMediaButton:
+                intent = new Intent(getApplicationContext(), ShareOnSocialMediaActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 
 
---
+++ app/src/main/java/com/example/dgurnani/placesihavebeentoapp/ShareOnSocialMediaActivity.java
@@ -0,0 +1,14 @@
+package com.example.dgurnani.placesihavebeentoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShareOnSocialMediaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_on_social_media);
+        setTitle("Social Media Services");
+    }
+}
